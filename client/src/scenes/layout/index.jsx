335ee97588015a9bb5ebb0bb7edb63314fd4c7a3
Major <107882188+qubitflip@users.noreply.github.com>
2023-06-08 09:00:36 -0500

    Sidebar work

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Box, useMediaQuery } from "@mui/material";
-import { Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
 
 const Layout = () => {
-  return (
-    <Box width="100%" height="100%">
-        <Navbar />
-        <Outlet />
-    </Box>
-  )
-}
+    const isNonMobile = useMediaQuery("(min-width: 600px)");
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    return (
+        <Box
+            display={isNonMobile ? "flex" : "block"}
+            width="100%"
+            height="100%"
+        >
+            <Sidebar
+                isNonMobile={isNonMobile}
+                drawerWidth="250px"
+                isSidebarOpen={isSidebarOpen}
+                setIsSidebarOpen={setIsSidebarOpen}
+            />
+            <Box>
+                <Navbar
+                    isSidebarOpen={isSidebarOpen}
+                    setIsSidebarOpen={setIsSidebarOpen}
+                />
+                <Outlet />
+            </Box>
+        </Box>
+    );
+};
 
-export default Layout
+export default Layout;
